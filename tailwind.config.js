6c93e0f59dcad745442240bad0475abd6b05a49d
HRHProjects <250752180+HRHProjects@users.noreply.github.com>
2026-01-11 21:28:50 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          200: '#99f6e4',
+          300: '#5eead4',
+          400: '#2dd4bf',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          800: '#115e59',
+          900: '#134e4a'
+        }
+      },
+      boxShadow: {
+        soft: '0 12px 34px rgba(2, 6, 23, 0.10)'
+      }
+    }
+  }
+}
